c5618e26268503a37b6ff185591a2496843c2271
DewanggaDh <73766205+DewanggaDh@users.noreply.github.com>
2020-11-12 20:57:17 +0700

    Login and Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" style="background-color:#F3F33B">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
